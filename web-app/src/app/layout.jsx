129dec910944f4de39f5257954541643bbd208c0
Rohit Deshmukh <deshmukhrohit373@gmail.com>
2026-04-13 17:55:11 +0000

    [ENABLED] Analytics

--- web-app/src/app/layout.jsx
+++ web-app/src/app/layout.jsx
@@ -1,4 +1,6 @@
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 import { getCurrentUser } from "@/lib/auth";
 import { UserProvider } from "@/providers/UserProvider";
 import NavbarWrapper from "@/components/NavbarWrapper";
